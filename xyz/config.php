dc9ae725b2fc7c013c4eeb0e326324261fb349d2
Vitaliy <liseenko.vitaliy.vladimirovich@gmail.com>
2023-04-04 01:51:00 +0300

    del config.php one more time

--- xyz/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config['login'] = 'admin';
-$config['pass_hash'] = '$2y$10$AZmTQq45SxLOCPSxyZxQxObxlEuIhuJ9g9CRQ.54lEz5ei8c.oNqq'; //use PASSWORD_BCRYPT
-
-$config['db_host'] = 'localhost';
-$config['db_name'] = 'chatgpt_admin';
-$config['db_user'] = 'root';
-$config['db_pass'] = '';
